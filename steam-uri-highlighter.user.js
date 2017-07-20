84821b980ad9c11e96612e7ad482cf106d2e3534
Timo Sand <timo.sand@iki.fi>
2017-07-20 18:19:33 +0300

    Create steam-uri-highlighter.user.js

---
+++ steam-uri-highlighter.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         GMail Steam URL highliter
+// @namespace    http://tampermonkey.net/
+// @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @version      1.0
+// @description  Replace Steam URI's in GMail with actual links
+// @author       deiga
+// @match        https://mail.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const steamUrlReplacer = function () {
+        const elem = Array.from(document.querySelectorAll('.gs div')).filter(function(item) { return item.textContent.indexOf('steam://') > -1; })[0];
+        if (elem) {
+            var elemHtml = elem.innerHTML;
+            if (elemHtml.indexOf('href="steam://') === -1) {
+                elem.innerHTML = elemHtml.replace(/steam:\/\/.*$/m,'<a href="$&">$&</a>');
+                elem.innerHTML = elem.innerHTML.replace('<wbr>','');
+            }
+        }
+    };
+
+    waitForKeyElements (".gs div:contains('steam://')", steamUrlReplacer);
+})();
